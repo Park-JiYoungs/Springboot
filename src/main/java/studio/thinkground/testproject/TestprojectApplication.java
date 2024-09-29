3468c99b24b3500ccfaf14f8f1f088311bf94783
Park JiYoungs <piy0305@gmail.com>
2024-09-29 15:16:51 +0900

    RestController,@GetMapping을 이용한 Hello World 출력

--- src/main/java/studio/thinkground/testproject/TestprojectApplication.java
+++ src/main/java/studio/thinkground/testproject/TestprojectApplication.java
@@ -1,7 +1,11 @@
 package studio.thinkground.testproject;
 
-public class Main {
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestprojectApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(TestprojectApplication.class, args);
     }
 }
